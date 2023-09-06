a39e4db627aca0c549cf4f74a4eb168ceab56154
Mahavirsinh Chauhan <mahavirsinh.chauhan@amoga.io>
2023-09-06 08:00:06 +0530

    protected resourses fetching added

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,30 @@
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 
 function WelcomeScreen() {
+  const [fetchedMsg, setFetchedMsg] = useState('');
+
+  const AuthCtx = useContext(AuthContext);
+  const token = AuthCtx.token;
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://rn-backend-fa2fb-default-rtdb.firebaseio.com/message.json?auth=${token}`
+      )
+      .then((res) => {
+        setFetchedMsg(res.data);
+      })
+      .catch((err) => console.error(err));
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMsg}</Text>
     </View>
   );
 }
